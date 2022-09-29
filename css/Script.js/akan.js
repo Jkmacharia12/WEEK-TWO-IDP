fdd2d66d514ab99ca049b46795401e5875a96f0d
Mash2 <mashjosephk01@gmail.com>
2022-09-29 06:56:25 +0300

    variables introduced on the js file

---
+++ css/Script.js/akan.js
@@ -0,0 +1,8 @@
+const Female_Names=[Akosua,Adwoa,Abena,Akua,Yaa,Efua,Amba]
+const Male_Names=[Kwasi,Kwadwo,Kwabena,Kwaku,Yaw,Kofi,Kwame]
+const Day_Of_Week=[Sunday,Monday,Tuesday,Wednesday,Thursday,Friday,Saturday]
+
+//user will be inputing date of birth and gender.
+//Based on the two parameters the application will determine DOW the user was born.
+//Return will be the user Akan name.
+
