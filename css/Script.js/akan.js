e4b3d7578238f78bfdf4c61a32fbb51d4904b0c3
Mash2 <mashjosephk01@gmail.com>
2022-09-30 01:18:09 +0300

    Date and month validators added

--- css/Script.js/akan.js
+++ css/Script.js/akan.js
@@ -14,7 +14,23 @@ function getAkanName () {
   
     let myGenderValue = getGender();
     console.log(myGenderValue);
-  
+  //Date validator
+ function dateValidator () { if (yearOfBirth%4===0 && monthOfBirth===2 && 1>dayOfBirth>28) {return false; } else
+   {return true;
+    
+  }if (1>dayOfBirth>31) { return false;
+    
+  } else { return true
+    
+  }}
+//Month Validator
+function monthValidator () {
+    if (monthOfBirth < 1 || monthOfBirth > 12) {
+      return false;
+    } else {
+      return true;
+    }
+  }
 
 const Female_Names=["Akosua","Adwoa","Abena","Akua","Yaa","Efua","Amba"]
 const Male_Names=["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"]
@@ -23,9 +39,8 @@ const Day_Of_Week=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","
 //user will be inputing date of birth and gender.
 //Based on the two parameters the application will determine DOW the user was born.
 //Return will be the user Akan name.
+
 var dow= function dayofweek(){Math.floor((((Number(yearOfBirth.slice(0,2))/4)-2*Number(yearOfBirth.slice(0,2))-1)+
           ((5*Number(yearOfBirth.slice(2,4))/4))+((26*(monthOfBirth+1)/10))+dayOfBirth)%7)}
-
-
 
 
