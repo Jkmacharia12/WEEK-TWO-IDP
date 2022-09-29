523ef6769a85481f580987006798502437224961
Mash2 <mashjosephk01@gmail.com>
2022-09-29 10:06:22 +0300

    more logic added

--- css/Script.js/akan.js
+++ css/Script.js/akan.js
@@ -1,8 +1,14 @@
-const Female_Names=[Akosua,Adwoa,Abena,Akua,Yaa,Efua,Amba]
-const Male_Names=[Kwasi,Kwadwo,Kwabena,Kwaku,Yaw,Kofi,Kwame]
-const Day_Of_Week=[Sunday,Monday,Tuesday,Wednesday,Thursday,Friday,Saturday]
+const Female_Names=["Akosua","Adwoa","Abena","Akua","Yaa","Efua","Amba"]
+const Male_Names=["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"]
+const Day_Of_Week=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
 
 //user will be inputing date of birth and gender.
 //Based on the two parameters the application will determine DOW the user was born.
 //Return will be the user Akan name.
+
+var Date=prompt("Enter date DD")
+var Month=prompt("Enter month MM")
+var year=prompt("Enter YYYY")
+var Gender=prompt("Enter Male/Female")
+
 
