758812ec38e5957a4a90a735276f273809effe6a
Mash2 <mashjosephk01@gmail.com>
2022-09-29 19:21:16 +0300

    New styles

--- css/Script.js/akan.js
+++ css/Script.js/akan.js
@@ -5,6 +5,15 @@ const Day_Of_Week=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","
 //user will be inputing date of birth and gender.
 //Based on the two parameters the application will determine DOW the user was born.
 //Return will be the user Akan name.
+var day=
+var month=
+var year=1980
+
+function Day of the week (CC,DD,MM,YY) {( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD )%7
+var CC=19
+var DD=30
+var MM=6
+var YY=84}
 
 
 
