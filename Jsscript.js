49e3a2553ca3ec0b26f2bfd5c3156237c5a37d14
Mash2 <mashjosephk01@gmail.com>
2022-10-01 12:00:53 +0300

    Date validation added

--- Jsscript.js
+++ Jsscript.js
@@ -8,6 +8,7 @@ const Male_Names=["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
     const days =document.getElementById("day-input");
     const male=document.getElementById("male-input");
     const female=document.getElementById("female-input");
+
     const date=`${year.value}-${month.value}-${days.value}`;
     const mydate = new Date(date);
     let day = mydate.getDay();
@@ -15,9 +16,18 @@ const Male_Names=["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
     
     if(male.checked){
         name=Male_Names[day];
-    }else{
+    }if(female.checked){
         name=Female_Names[day];
+        //date validation
+    }if (days.value>31||month.value>12){
+        name="You entered an Invalid Date";
+}if (month.value==2&&days.value>28){name="You entered an Invalid Date";
+}if (year.value%4==0&&month.value==2&&days.value>29){name="You entered an Invalid Date";
+}
+    else{
+        //name=Female_Names[day];
     }
+    year.value%4==0; if (month.value==2&&days.value>29){System.out.println("You entered an Invalid Date")}
     alert(name);
     let nameElement=document.getElementById("result");
     nameElement.innerHTML(name);
